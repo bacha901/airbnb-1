029e449619701dd9847b42929301992f0fca119c
Hossam <hossamfathi050@outlook.com>
2021-01-08 18:09:46 +0300

    Add Code For Views Property App

--- property/views.py
+++ property/views.py
@@ -1,3 +1,19 @@
+
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView , DetailView
+from .models import Property
+
+
+
+class PropertyList(ListView):
+    model = Property
+    ## filter
+    ## pagination 
+
+class PropertyDetail(DetailView):
+    model = Property
+
+    ## book 
