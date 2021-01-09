1d945e76f21bdfcecd6b4ab749436ea533dd92f1
Hossam <hossamfathi050@outlook.com>
2021-01-09 16:05:13 +0300

    Add Code in Admin Property App

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Property , PropertyBook , PropertyImages , PropertyReview , Category , Place
+
+
+
+admin.site.register(Property)
+admin.site.register(PropertyBook)
+admin.site.register(PropertyImages)
