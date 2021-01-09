7f56e908a6fb62f0441239d0912f485a1308a40a
Hossam <hossamfathi050@outlook.com>
2021-01-09 16:06:32 +0300

    Add Moed Code in Admin Property App

--- property/admin.py
+++ property/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from . models import Property , PropertyBook , PropertyImages , PropertyReview , Category , Place
 
 
-
 admin.site.register(Property)
 admin.site.register(PropertyBook)
 admin.site.register(PropertyImages)
+admin.site.register(PropertyReview)
+admin.site.register(Category)
+admin.site.register(Place)
