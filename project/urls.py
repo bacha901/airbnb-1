cb5f4ab1a8d7a21f290ad9c604edd763a30f8684
Hossam <hossamfathi050@outlook.com>
2021-01-10 16:09:19 +0300

    Add code for main urls urlpatterns for media and ststic

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""
 from django.urls import path , include
 from django.contrib import admin
 from django.conf import settings
@@ -23,3 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('property/' , include('property.urls' , namespace='property'))
 ]
+
+
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
