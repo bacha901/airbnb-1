4a808d888b1d05851a4b720c12b10df73239aca1
Hossam <hossamfathi050@outlook.com>
2021-01-16 21:43:13 +0300

    Add More Code More To blog app - urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,3 +3,11 @@ from . import views
 
 
 app_name = 'blog'
+
+urlpatterns = [
+    path('' , views.PostList.as_view(),name='post_list'),
+    path('<slug:slug>' ,views.PostDetail.as_view(),name='post_detail' ),
+
+    path('category/<str:slug>' ,views.PostsByCategory.as_view(),name='post_by_category' ),
+    path('tags/<slug:slug>' ,views.PostsByTags.as_view(),name='post_by_tags' ),
+]
