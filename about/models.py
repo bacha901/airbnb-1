05390bc90dc87f68b6fb131a73634ddc61033c0b
Hossam <hossamfathi050@outlook.com>
2021-01-22 22:34:35 +0300

    model about - Create your model

--- about/models.py
+++ about/models.py
@@ -10,11 +10,10 @@ class About(models.Model):
     def __str__(self):
         return str(self.id)
 
-
-
 class FAQ(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField(max_length=3000)
 
     def __str__(self):
         return self.title
+
